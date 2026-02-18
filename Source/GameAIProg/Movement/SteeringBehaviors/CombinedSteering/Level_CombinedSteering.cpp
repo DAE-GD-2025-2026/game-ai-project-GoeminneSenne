8f585f11f2d54b9f238eb64c80b304fb50339488
GoeminneSenne <Senne.goeminne@student.howest.be>
2026-02-18 17:15:26 +0100

    removed agent functions from level
    Instead now just create 1 agent which wanders

--- Source/GameAIProg/Movement/SteeringBehaviors/CombinedSteering/Level_CombinedSteering.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/CombinedSteering/Level_CombinedSteering.cpp
@@ -24,6 +24,8 @@ void ALevel_CombinedSteering::BeginPlay()
 	WeightedBehaviors.emplace_back(pWander.get(), 0.5f);
 	pBlendedSteering = std::make_unique<BlendedSteering>(WeightedBehaviors);
 	
+	pDrunkAgent = GetWorld()->SpawnActor<ASteeringAgent>(SteeringAgentClass, FVector{0,0,90}, FRotator::ZeroRotator);
+	pDrunkAgent->SetSteeringBehavior(pWander.get());
 }
 
 void ALevel_CombinedSteering::BeginDestroy()
@@ -106,133 +108,10 @@ void ALevel_CombinedSteering::Tick(float DeltaTime)
 		ImGui::End();
 	}
 #pragma endregion
-	
-	for (ImGui_Agent& a : SteeringAgents)
-	{
-		if (a.Agent)
-		{
-			UpdateTarget(a);
-		}
-	}
+
 	
 	// Combined Steering Update
  // TODO: implement handling mouse click input for seek
  // TODO: implement Make sure to also evade the wanderer
 }
 
-bool ALevel_CombinedSteering::AddAgent(BehaviorTypes BehaviorType, bool AutoOrient)
-{
-	ImGui_Agent ImGuiAgent = {};
-	ImGuiAgent.Agent = GetWorld()->SpawnActor<ASteeringAgent>(SteeringAgentClass, FVector{0,0,90}, FRotator::ZeroRotator);
-	if (IsValid(ImGuiAgent.Agent))
-	{
-		ImGuiAgent.SelectedBehavior = static_cast<int>(BehaviorType);
-		ImGuiAgent.SelectedTarget = -1; // Mouse
-		
-		SetAgentBehavior(ImGuiAgent);
-
-		SteeringAgents.push_back(std::move(ImGuiAgent));
-		
-		RefreshTargetLabels();
-
-		return true;
-	}
-
-	return false;
-}
-
-void ALevel_CombinedSteering::RemoveAgent(unsigned int Index)
-{
-	SteeringAgents[Index].Agent->Destroy();
-	SteeringAgents.erase(SteeringAgents.begin() + Index);
-
-	RefreshTargetLabels();
-	RefreshAgentTargets(Index);
-}
-
-void ALevel_CombinedSteering::SetAgentBehavior(ImGui_Agent& Agent)
-{
-	Agent.Behavior.reset();
-	
-	
-	switch (static_cast<BehaviorTypes>(Agent.SelectedBehavior))
-	{
-	case BehaviorTypes::Seek:
-		Agent.Behavior = std::make_unique<Seek>();
-		break;
-	case BehaviorTypes::Flee:
-		Agent.Behavior = std::make_unique<Flee>();
-		break;
-	case BehaviorTypes::Arrive:
-		Agent.Behavior = std::make_unique<Arrive>();
-		break;
-	case BehaviorTypes::Face:
-		Agent.Behavior = std::make_unique<Face>();
-		break;
-	case BehaviorTypes::Pursuit:
-		Agent.Behavior = std::make_unique<Pursuit>();
-		break;
-	case BehaviorTypes::Evade:
-		Agent.Behavior = std::make_unique<Evade>();
-		break;
-	case BehaviorTypes::Wander:
-		Agent.Behavior = std::make_unique<Wander>();
-		break;
-	default:
-		assert(false); // Incorrect Agent Behavior gotten during SetAgentBehavior()	
-	} 
-	
-
-	UpdateTarget(Agent);
-	
-	Agent.Agent->SetSteeringBehavior(Agent.Behavior.get());
-}
-
-void ALevel_CombinedSteering::RefreshTargetLabels()
-{
-	TargetLabels.clear();
-	
-	TargetLabels.push_back("Mouse");
-	for (int i{0}; i < SteeringAgents.size(); ++i)
-	{
-		TargetLabels.push_back(std::format("Agent {}", i));
-	}
-}
-
-void ALevel_CombinedSteering::UpdateTarget(ImGui_Agent& Agent)
-{
-	// Note: MouseTarget position is updated via Level BP every click
-	
-	bool const bUseMouseAsTarget = Agent.SelectedTarget < 0;
-	if (!bUseMouseAsTarget)
-	{
-		ASteeringAgent* const TargetAgent = SteeringAgents[Agent.SelectedTarget].Agent;
-
-		FTargetData Target;
-		Target.Position = TargetAgent->GetPosition();
-		Target.Orientation = TargetAgent->GetRotation();
-		Target.LinearVelocity = TargetAgent->GetLinearVelocity();
-		Target.AngularVelocity = TargetAgent->GetAngularVelocity();
-
-		Agent.Behavior->SetTarget(Target);
-	}
-	else
-	{
-		Agent.Behavior->SetTarget(MouseTarget);
-	}
-}
-
-void ALevel_CombinedSteering::RefreshAgentTargets(unsigned int IndexRemoved)
-{
-	for (UINT i = 0; i < SteeringAgents.size(); ++i)
-	{
-		if (i >= IndexRemoved)
-		{
-			auto& Agent = SteeringAgents[i];
-			if (Agent.SelectedTarget == IndexRemoved || i  == Agent.SelectedTarget)
-			{
-				--Agent.SelectedTarget;
-			}
-		}
-	}
-}
--- Source/GameAIProg/Movement/SteeringBehaviors/CombinedSteering/Level_CombinedSteering.h
+++ Source/GameAIProg/Movement/SteeringBehaviors/CombinedSteering/Level_CombinedSteering.h
@@ -34,43 +34,10 @@ private:
 	//Datamembers
 	bool UseMouseTarget = false;
 	bool CanDebugRender = false;
-
-	enum class BehaviorTypes
-	{
-		Seek,
-		Wander,
-		Flee,
-		Arrive,
-		Evade,
-		Pursuit,
-		Face,
-
-		// @ End
-		Count
-	};
-
-	struct ImGui_Agent final
-	{
-		ASteeringAgent* Agent{nullptr};
-		std::unique_ptr<ISteeringBehavior> Behavior{nullptr};
-		int SelectedBehavior{static_cast<int>(BehaviorTypes::Seek)};
-		int SelectedTarget = -1;
-	};
-	
-	std::vector<ImGui_Agent> SteeringAgents{};
-	std::vector<std::string> TargetLabels{};
 	
 	std::unique_ptr<Seek> pSeek{};
 	std::unique_ptr<Wander> pWander{};
 	std::unique_ptr<BlendedSteering> pBlendedSteering{};
 	
-	int AgentIndexToRemove = -1;
-	
-	bool AddAgent(BehaviorTypes BehaviorType = BehaviorTypes::Wander, bool AutoOrient = true);
-	void RemoveAgent(unsigned int Index);
-	void SetAgentBehavior(ImGui_Agent& Agent);
-
-	void RefreshTargetLabels();
-	void UpdateTarget(ImGui_Agent& Agent);
-	void RefreshAgentTargets(unsigned int IndexRemoved);
+	ASteeringAgent* pDrunkAgent{};
 };
