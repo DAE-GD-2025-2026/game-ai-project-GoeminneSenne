91e4191e971b4182397783079988e2edabc727d1
GoeminneSenne <Senne.goeminne@student.howest.be>
2026-04-22 15:13:31 +0200

    Added State, Transition & FSM class

---
+++ Source/GameAIProg/DecisionMaking/FSM/FSM.cpp
@@ -0,0 +1,6 @@
+﻿#include "FSM.h"
+
+void GAMEAI::FSM::FSM::SetBlackboard(UBlackboardComponent* Blackboard)
+{
+	this->Blackboard = Blackboard;
+}
---
+++ Source/GameAIProg/DecisionMaking/FSM/FSM.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+#include <functional>
+#include <memory>
+#include <vector>
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+namespace GAMEAI::FSM
+{
+	class State
+	{
+		virtual ~State() = default;
+	};
+	
+	class Transition
+	{
+	public:
+		
+	private:
+		State* FromState{};
+		State* ToState{};
+		std::function<bool()> EvalFunc{};
+	};
+	
+	class FSM
+	{
+	public:
+		void SetBlackboard(UBlackboardComponent* Blackboard);
+		
+	private:
+		State* CurrentState{};
+		UBlackboardComponent* Blackboard{};
+		
+		std::vector<std::unique_ptr<State>> States;
+		std::vector<std::unique_ptr<Transition>> Transitions;
+	};
+}
