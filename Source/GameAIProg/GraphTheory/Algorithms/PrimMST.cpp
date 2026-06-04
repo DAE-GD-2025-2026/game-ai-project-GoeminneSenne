830a7d53138cd82be8e83aae1749c389d0483ed2
GoeminneSenne <Senne.goeminne@student.howest.be>
2026-06-04 22:06:31 +0200

    Implemented Prim algorithm

--- Source/GameAIProg/GraphTheory/Algorithms/PrimMST.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/PrimMST.cpp
@@ -2,25 +2,58 @@
 
 void GameAI::PrimMST::CalculateMST(GameAI::Graph& Graph)
 {
-	Visited.clear();
-	Visited.insert(Graph.GetNode(0).get());
-	
 	// A graph with 1 node is always an MST
 	if (Graph.GetNodeCount() == 1)
 	{
 		return;
 	}
 	
-	auto conn = FindCheapestConnection(Graph);
-	UE_LOG(LogTemp, Log, TEXT("Cheapest connection has weight %f"), conn->GetWeight());
+	Visited.clear();
+	std::unordered_set<Connection*> MstEdges;
 	
+	//Start construction from the first node in the graph
+	Visited.insert(Graph.GetNode(0).get());
+	
+	while (Visited.size() < Graph.GetNodeCount())
+	{
+		Connection* CheapestConn = FindCheapestConnection(Graph);
+		
+		//If no edge is found the graph is disconnected
+		if (CheapestConn == nullptr)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Graph is disconnected, cannot construct MST"));
+			return;
+		}
+		
+		//Add new node to visited
+		// & new edge to MSTEdges
+		MstEdges.insert(CheapestConn);
+		Visited.insert(Graph.GetNode(CheapestConn->GetToId()).get());
+		
+		//If graph is bidirectional, also add the inverse connection to MSTEdges
+		if (Graph.GetIsDirectional())
+		{
+			auto InverseConnection = Graph.FindConnection(CheapestConn->GetToId(), CheapestConn->GetFromId());
+			MstEdges.insert(InverseConnection);
+		}
+	}
+	
+	//Remove all edges that are not in MstEdges
+	//auto& Connections = Graph.GetConnections();
+	auto Removed = std::ranges::remove_if( Graph.GetConnections(),
+		[&](const std::unique_ptr<Connection>& Conn)
+		{
+			return not MstEdges.contains(Conn.get());
+		});
+	
+	 Graph.GetConnections().erase(Removed.begin(), Removed.end());
 }
 
 GameAI::Connection* GameAI::PrimMST::FindCheapestConnection(const GameAI::Graph& Graph)
 {
 	std::vector<GameAI::Connection*> Candidates;
 	
-	//Gather all candidate edges (ones that connect a unvisited node to a visited node)
+	//Gather all candidate edges (ones that connect an unvisited node to a visited node)
 	for (Node* VisitedNode : Visited)
 	{
 		auto ConnectionsFrom = Graph.FindConnectionsFrom(VisitedNode->GetId());
